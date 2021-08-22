14dfa9dae6b777fb9489a2205989632f721c0435
Nagah <E-n.Shinawy@lean.sa>
2021-08-22 22:06:56 +0300

    delete image path whenever product is deleted. adding logging

--- apps/store/models.py
+++ apps/store/models.py
@@ -1,9 +1,13 @@
+import os
+import logging
 from django.db import models
-from django.db.models.signals import post_save, pre_save
+from django.db.models.signals import post_save, pre_save, pre_delete
 from django.dispatch import receiver
 from django.core.validators import MaxValueValidator, MinValueValidator
 from .receivers import notify_image_receiver, discount_price_receiver
 
+
+logger = logging.getLogger(__name__)
 
 # https://dev.to/epamindia/django-signals-30g3
 
@@ -13,7 +17,9 @@ class Product(models.Model):
     image = models.ImageField(null=True, blank=True, upload_to="products/%Y-%m-%d")
     created = models.DateTimeField(auto_now_add=True)
     price = models.DecimalField(max_digits=12, decimal_places=2, default=1000)
-    offer = models.PositiveIntegerField(default=0, validators=[MaxValueValidator(100), MinValueValidator(0)])
+    offer = models.PositiveIntegerField(
+        default=0, validators=[MaxValueValidator(100), MinValueValidator(0)]
+    )
 
     def __str__(self):
         if self.price:
@@ -36,7 +42,7 @@ def create_product(sender, instance, **kwargs):
     :return:
     """
     if kwargs.get("created"):  # True just for first time when obj created
-        print(f"Emails send to user with new product <{instance}>")
+        logger.info(f"Emails send to user with new product <{instance}>")
 
 
 @receiver(pre_save, sender=Product)
@@ -52,4 +58,15 @@ def dropped_price(sender, instance, **kwargs):
     current_instance = instance
     saved_instance = sender.objects.get(id=instance.id)
     if saved_instance.price < current_instance.price:
-        print(f"You Are Changing Price From '{saved_instance.price}' to '{current_instance.price}'")
+        logger.info(
+            f"You Are Changing Price From '{saved_instance.price}' to '{current_instance.price}'"
+        )
+
+
+@receiver(pre_delete, sender=Product)
+def delete_product_image(sender, instance, **kwargs):
+    if not instance.image:
+        return
+    imgpath = f"{instance.image.path}"
+    os.remove(imgpath)
+    logger.info(f"Image of <{instance}> at path <{imgpath}> was deleted")
