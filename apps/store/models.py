5ce2172db14e6172a5cb1d6be25b56c9f1e3920d
Nagah <E-n.Shinawy@lean.sa>
2021-08-22 20:47:18 +0300

    signals with dispatch receiver

---
+++ apps/store/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+# https://dev.to/epamindia/django-signals-30g3
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=256)
+    image = models.ImageField(null=True, blank=True, upload_to="products/%Y-%m-%d")
+    created = models.DateTimeField(auto_now_add=True)
+    price = models.DecimalField(max_digits=12, decimal_places=2, blank=True, null=True)
+
+    def __str__(self):
+        if self.price:
+            return f"{self.title} - {self.price}$"
+        return self.title
+
+
+@receiver(post_save, sender=Product)
+def create_product(sender, instance, **kwargs):
+    """
+    post_save signal sends other user useful argument called created
+    which is useful to understand if the object is being created or updated.
+    :param sender:
+    :param instance:
+    :param kwargs:
+    :return:
+    """
+    if kwargs.get("created"):  # True just for first time when obj created
+        print(f"Emails send to user with new product {instance}")
+
