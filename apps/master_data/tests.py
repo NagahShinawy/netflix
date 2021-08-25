1622d3f15224265a3b14e66cf60256020bab97fe
Nagah <E-n.Shinawy@lean.sa>
2021-08-25 17:13:01 +0300

    tests for city & country

--- apps/master_data/tests.py
+++ apps/master_data/tests.py
@@ -1,3 +1,39 @@
 from django.test import TestCase
+from .models import Country, City
 
-# Create your tests here.
+
+class TestCountry(TestCase):
+
+    def setUp(self) -> None:
+        self.egypt = Country.objects.create(name="Egypt")
+
+        self.cairo = City.objects.create(name="Cairo", country=self.egypt)
+        self.alex = City.objects.create(name="Alex", country=self.egypt)
+        self.sharm = City.objects.create(name="Sharm", country=self.egypt)
+
+    def test_create_country(self):
+        self.assertEqual(Country.objects.all().count(), 1)
+
+    def test_country_name(self):
+        self.assertEqual(self.egypt.name, "Egypt")
+
+    def test_cities_related(self):
+        cities = self.egypt.cities.all()
+        self.assertEqual(cities.count(), 3)
+
+
+class TestCity(TestCase):
+
+    def setUp(self) -> None:
+        self.su = Country.objects.create(name="Su Arabia")
+        self.riyad = City.objects.create(name="Riyad", country=self.su)
+
+    def test_create_city(self):
+        self.assertEqual(City.objects.all().count(), 1)
+
+    def test_city_name(self):
+        self.assertEqual(self.riyad.name, "Riyad")
+
+    def test_country_of_city(self):
+        self.assertEqual(self.riyad.country, self.su)
+
