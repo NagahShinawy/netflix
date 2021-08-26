171a1a849835754a11d1d848b1654703a3798d6f
Nagah <E-n.Shinawy@lean.sa>
2021-08-26 17:29:29 +0300

    abstract school models

---
+++ apps/school/models.py
@@ -0,0 +1,50 @@
+from django.db import models
+
+
+# ########### # ############ Abstract Models ############ ############ ############ ############ ############
+
+class Person(models.Model):
+    name = models.CharField(max_length=256)
+    dob = models.DateTimeField(verbose_name="Date of Birth")
+    joined_date = models.DateTimeField(auto_now_add=True)
+    address = models.CharField(max_length=256, null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.name} - {self.dob}"
+
+    class Meta:
+        abstract = True
+
+
+class SchoolEmployee(Person):
+    compensation = models.CharField(max_length=256)
+
+    class Meta:
+        abstract = True
+
+
+class HiredHelp(SchoolEmployee):
+    shift_timings = models.JSONField()
+
+    class Meta:
+        abstract = True
+
+
+# ########### # ############ Actual Models ############ ############ ############ ############ ############
+
+
+class Student(Person):
+    roll_num = models.IntegerField()
+
+
+class Teacher(SchoolEmployee):
+    qualifications = models.JSONField()
+
+
+class CleaningMember(HiredHelp):
+    pass
+
+
+
+
+
