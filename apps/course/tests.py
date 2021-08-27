4329fea0096534795d96297d54c9ffc1016d96de
Nagah <E-n.Shinawy@lean.sa>
2021-08-27 10:43:55 +0300

    medical_centers tests

--- apps/course/tests.py
+++ apps/course/tests.py
@@ -2,8 +2,7 @@ from django.test import TestCase
 from .models import MedicalCenter, Doctor
 
 
-class DoctorModelTestCase(TestCase):
-
+class DoctorCenterTestCaseMixin(TestCase):
     def setUp(self) -> None:
         self.mayo = MedicalCenter.objects.create(name="Mayo Clinic")
         self.toronto = MedicalCenter.objects.create(name="Toronto Clinic")
@@ -11,6 +10,9 @@ class DoctorModelTestCase(TestCase):
         self.john = Doctor.objects.create(fname="John")
         self.john.medical_centers.set(self.medical_centers)
         self.john.save()
+
+
+class DoctorModelTestCase(DoctorCenterTestCaseMixin):
 
     def test_add_doctor(self):
         self.assertEqual(Doctor.objects.all().count(), 1)
@@ -23,6 +25,22 @@ class DoctorModelTestCase(TestCase):
         self.assertEqual(john_centers.count(), len(self.medical_centers))
         self.assertEqual(john_centers.first(), self.mayo)
         self.assertEqual(john_centers.first().name, self.mayo.name)
+
+
+class MedicalCenterTestCase(DoctorCenterTestCaseMixin):
+
+    def test_create_medical_centers(self):
+        self.assertEqual(MedicalCenter.objects.all().count(), len(self.medical_centers))
+
+    def test_doctors_joined_to_medical_centers(self):
+        doctors = Doctor.objects.all()
+        mayo_doctors = self.mayo.doctors.all()
+        toronto_doctors = self.toronto.doctors.all()
+        self.assertEqual(mayo_doctors.count(), doctors.count())
+        self.assertEqual(toronto_doctors.count(), doctors.count())
+
+
+
 
 
 
